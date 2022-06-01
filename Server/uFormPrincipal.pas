057d1b1ce8da2db23e52519b999df04015ecc55d
Bruno Siqueira <oxalangababu@gmail.com>
2022-05-31 23:26:51 -0400

    New classes TUserThreadList and TServerThread

--- Server/uFormPrincipal.pas
+++ Server/uFormPrincipal.pas
@@ -5,10 +5,16 @@ unit uFormPrincipal;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  uThreadClasses;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
   private
 
   public
@@ -21,6 +27,19 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  Server: TServerThread;
+begin
+  Server           := TServerThread.Create(True);
+  Server.IPAddress := '127.0.0.1';
+  Server.Port      := '3270';
+  Server.TimeOut   := 10000;
+  Server.Start;
+end;
 
 end.
 
--- uThreadClasses.pas
+++ uThreadClasses.pas
@@ -5,21 +5,21 @@ unit uThreadClasses;
 interface
 
 uses
-  Classes, SysUtils, blcksock, synsock, fpjson, jsonparser;
+  Classes, SysUtils, contnrs, blcksock, synsock, fpjson, jsonparser;
 
 type
-
   { TUserThread }
-
   TUserThread = class(TThread)
   strict private
     FBlockSocekt: TTCPBlockSocket;
-    FStop: Boolean;
-    FTimeOut: Integer;
+    FTimeOut    : Integer;
   strict private
     procedure SetSocket(const AValue: TSocket);
     procedure SendFileList;
     procedure SendNewFiles;
+    procedure SendOpError;
+  strict protected
+    property FreeOnTerminate;
   protected
     procedure Execute; override;
   public
@@ -27,15 +27,142 @@ type
     destructor Destroy; override;
 
     property Socket: TSocket write SetSocket;
-    property Stop: Boolean read FStop write FStop;
     property TimeOut: Integer read FTimeOut write FTimeOut;
   end;
+
+  { TUserThreadList }
+  TUserThreadList = class
+  strict private
+    FList: TObjectList;
+  strict private
+    function GetItems(AIndex: Integer): TUserThread;
+  public
+    constructor Create;
+    destructor Destroy; override;
+
+    function Add(const ASock: TSocket): Integer;
+    function Count: Integer;
+
+    procedure StopThreads;
+    property Items[AIndex: Integer]: TUserThread read GetItems; default;
+  end;
+
+  { TServerThread }
+
+  TServerThread = class(TThread)
+  strict private
+    FBlockSocket: TTCPBlockSocket;
+    FList       : TUserThreadList;
+    FIPAddress  : string;
+    FPort       : string;
+    FTimeOut    : Integer;
+  strict protected
+    property FreeOnTerminate;
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(const AFreeOnTerminate: Boolean);
+    destructor Destroy; override;
+
+    property IPAddress: string read FIPAddress write FIPAddress;
+    property Port: string read FPort write FPort;
+    property TimeOut: Integer read FTimeOut write FTimeOut;
+  end;
+
 
 implementation
 
 const
   OPERATION_GET_LIST = 1;
   OPERATION_GET_FILE = 2;
+
+{ TServerThread }
+
+procedure TServerThread.Execute;
+var
+  I: Integer;
+begin
+  FBlockSocket.CreateSocket;
+  FBlockSocket.Bind(FIPAddress, FPort);
+  FBlockSocket.Listen;
+  try
+    if FBlockSocket.LastError = 0 then
+    begin
+      while not Terminated do
+      begin
+        if FBlockSocket.CanRead(1000) then
+        begin
+          I := FList.Add(FBlockSocket.Accept);
+          if FBlockSocket.LastError = 0 then
+            FList[I].Start
+          else
+            FList[I]; { Deletar item inserido }
+        end;
+      end;
+    end;
+  finally
+    FBlockSocket.CloseSocket;
+    FBlockSocket.Purge;
+  end;
+end;
+
+constructor TServerThread.Create(const AFreeOnTerminate: Boolean);
+begin
+  inherited Create(True);
+  FBlockSocket    := TTCPBlockSocket.Create;
+  FList           := TUserThreadList.Create;
+  FreeOnTerminate := AFreeOnTerminate;
+end;
+
+destructor TServerThread.Destroy;
+begin
+  FList.Free;
+  FBlockSocket.CloseSocket;
+  FBlockSocket.Free;
+  inherited Destroy;
+end;
+
+{ TUserThreadList }
+
+function TUserThreadList.GetItems(AIndex: Integer): TUserThread;
+begin
+  Result := FList.Items[AIndex] as TUserThread;
+end;
+
+procedure TUserThreadList.StopThreads;
+var
+  I: Integer;
+  Thread: TUserThread;
+begin
+  for I := 0 to FList.Count -1 do
+  begin
+    Thread := Items[I];
+    Thread.Terminate;
+    Thread.WaitFor;
+  end;
+end;
+
+constructor TUserThreadList.Create;
+begin
+  FList := TObjectList.Create(True);
+end;
+
+destructor TUserThreadList.Destroy;
+begin
+  StopThreads;
+  FList.Free;
+  inherited Destroy;
+end;
+
+function TUserThreadList.Add(const ASock: TSocket): Integer;
+begin
+  Result := FList.Add(TUserThread.Create(ASock));
+end;
+
+function TUserThreadList.Count: Integer;
+begin
+  Result := FList.Count;
+end;
 
 { TUserThread }
 procedure TUserThread.SetSocket(const AValue: TSocket);
@@ -53,11 +180,16 @@ begin
 
 end;
 
+procedure TUserThread.SendOpError;
+begin
+
+end;
+
 procedure TUserThread.Execute;
 var
   Request: string;
 begin
-  while (not FStop) and (not Terminated) do
+  while not Terminated do
   begin
     Request := FBlockSocekt.RecvString(FTimeOut);
     if FBlockSocekt.LastError <> 0 then
@@ -68,6 +200,8 @@ begin
       case Integers['operation'] of
         OPERATION_GET_LIST: SendFileList;
         OPERATION_GET_FILE: SendNewFiles;
+      else
+        SendOpError;
       end;
     end;
   end;
@@ -76,9 +210,9 @@ end;
 constructor TUserThread.Create(const ASocket: TSocket);
 begin
   inherited Create(True);
-  FBlockSocekt := TTCPBlockSocket.Create;
-  FreeOnTerminate := True;
+  FBlockSocekt        := TTCPBlockSocket.Create;
   FBlockSocekt.Socket := ASocket;
+  FreeOnTerminate     := False;
 end;
 
 destructor TUserThread.Destroy;
